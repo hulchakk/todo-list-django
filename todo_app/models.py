5ce9f176e884c522106145f5c45a4bb9e82837e4
Dmytro <ys33ys33ys55@gmail.com>
2026-03-05 17:17:19 +0100

    feat(todo_app): implement Task and Tag models

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=255,
+    )
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(
+        auto_created=True,
+    )
+    deadline = models.DateTimeField(
+        blank=True,
+        null=True,
+    )
+    is_done = models.BooleanField(
+        default=False,
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        related_name="tasks",
+    )
