66bfb7103799d2dc554666cecdff571a8948a01a
Dmytro <ys33ys33ys55@gmail.com>
2026-03-05 20:03:54 +0100

    feat(todo_app): implement task status change

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,12 +3,18 @@ from django.urls import path
 from todo_app.views import (
     TaskListView,
     TaskCreateView,
+    task_change_status,
 )
 
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="todo-list"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path(
+        "tasks/<int:pk>/change_status",
+        task_change_status,
+        name="task-change-status"
+    )
 ]
 
 
