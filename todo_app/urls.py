ae62466402e0e1c2f0f40640703b92059b32d90c
Dmytro <ys33ys33ys55@gmail.com>
2026-03-05 17:36:41 +0100

    feat(todo_app):  implement home page

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
+from todo_app.views import TaskListView
+
 
 urlpatterns = [
-    
+    path("", TaskListView.as_view(), name="todo-list")
 ]
 
 
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from todo_app.models import (
+    Task,
+    Tag,
+)
+
+
+class TaskListView(generic.ListView):
+    model = Task
